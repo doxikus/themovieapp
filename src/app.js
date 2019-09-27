4ac0009544c4794dca572023238403aa000c8396
Stanislav Mihic <stamba@Stanislavs-MacBook-Pro-2.local>
2019-09-27 22:12:26 +0200

    First commit :-), setup dev server

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="movies">
+      <h1>Test Server Hello !</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      // Rules to load scss
+      {
+        // Some change here
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+        }
+    ]
+  },
+  // change line
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  }
+};
